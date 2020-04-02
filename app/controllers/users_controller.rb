98993357959080a7d7cd3c392eaf04f23e62d4b1
taro.kirameki <taro.kirameki@techacademy.jp>
2020-04-02 15:27:58 +0000

    two commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
